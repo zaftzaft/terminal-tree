098cbaabc687035d0f7c98853fcc258aaeed068d
@zaftzaft <zaftzaft7@gmail.com>
2015-04-11 13:48:44 +0900

    add 'highlight' and 'colors' options

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-function isObject(o){
-  return !Array.isArray(o) && o === new Object(o);
+var chalk = require("chalk");
+
+function isFunction(f){
+  return Object.prototype.toString.call(f) === "[object Function]";
 }
+
+function isRegExp(r){
+  return r instanceof RegExp;
+}
+
+function isObject(o){
+  return !Array.isArray(o) && !isFunction(o) && !isRegExp(o) && o === new Object(o);
+}
+
+var reColorName = /^(black|red|green|yellow|blue|magenta|cyan|white|gray)$/;
+function coloring(str, color){
+  str = str + "";
+  if(typeof color === "function"){
+    return color(str);
+  }
+  else if(reColorName.test(color)){
+    return chalk[color](str);
+  }
+  else{
+    return str;
+  }
+};
+
+var defaultColors = {
+  "string": "yellow",
+  "number": "red",
+  "boolean": "blue",
+  "infinity": "red",
+  "nan": "cyan",
+  "null": "cyan",
+  "undefined": "gray",
+  "regexp": "green",
+  "key": "magenta",
+  "object": "white",
+  "array": "white"
+};
 
 
 module.exports = function(json, options){
@@ -10,17 +48,23 @@ module.exports = function(json, options){
   }
   options.padding = options.padding || 0;
   options.markdown = options.markdown || false;
+  options.highlight = options.highlight || false;
+  options.colors = options.colors || {};
+
+  Object.keys(defaultColors).forEach(function(key){
+    options.colors[key] = options.colors[key] || defaultColors[key];
+  });
 
   var output = "";
-  var tree = function(o, depth, index, isLast, inArray){
+  var tree = function(o, depth, index, isLast, inArray, type){
     index = [].concat(index || []);
     depth = depth || 0;
     index[depth] = 0;
-  
+
     if(Array.isArray(o)){
       if(inArray){
         if(options.symbol){
-          tree("[]", depth, index, isLast);
+          tree("[]", depth, index, isLast, 0, "array");
           index[depth] = isLast ? 0 : 1;
         }
         else{
@@ -35,7 +79,7 @@ module.exports = function(json, options){
       var keys = Object.keys(o);
       if(inArray){
         if(options.symbol){
-          tree("{}", depth, index, isLast);
+          tree("{}", depth, index, isLast, 0, "object");
           index[depth] = isLast ? 0 : 1;
           depth++;
         }
@@ -43,19 +87,23 @@ module.exports = function(json, options){
       for(var i = 0, l = keys.length;i < l;i++){
         var lastFlg = (inArray && !isLast && !options.symbol) ? false : (i === l - 1);
 
-        tree(keys[i], depth, index, lastFlg);
+        tree(keys[i], depth, index, lastFlg, 0, "key");
         index[depth] = lastFlg ? 0 : 1;
         tree(o[keys[i]], depth + 1, index, 1, 1);
       }
     }
     else{
       index[depth] = isLast ? 3 : 2;
+      if(isFunction(o)){
+        o = "Function";
+        type = "function";
+      }
 
       if(options.markdown){
         output += markdownList(depth) + o + "\n";
       }
       else{
-        output += format(index) + o + "\n";
+        output += format(index) + highlight(o, type) + "\n";
       }
 
     }
@@ -80,6 +128,49 @@ module.exports = function(json, options){
     return new Array(depth + 1).join("  ") + "+ ";
   };
 
+  var highlight = function(data, type){
+    var hi;
+    if(!options.highlight){
+      return data;
+    }
+    if(type === "key"){
+      hi = "key";
+    }
+    else if(type === "array"){
+      hi = "array";
+    }
+    else if(type === "object"){
+      hi = "object";
+    }
+    else if(data !== data){
+      hi = "nan";
+    }
+    else if(data === Infinity){
+      hi = "infinity";
+    }
+    else if(typeof data === "string"){
+      hi = "string";
+    }
+    else if(typeof data === "number"){
+      hi = "number";
+    }
+    else if(isRegExp(data)){
+      hi = "regexp";
+    }
+    else if(typeof data === "boolean"){
+      hi = "boolean";
+    }
+    else if(data === null){
+      hi = "null";
+    }
+    else if(data === void 0){
+      hi = "undefined";
+    }
+    else{
+      return data;
+    }
+    return coloring(data, options.colors[hi]);
+  };
 
   tree(json);
   return output;
