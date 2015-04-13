5277dfded96e2f3106e54526c389a9fcfb5c62ec
@zaftzaft <zaftzaft7@gmail.com>
2015-04-13 12:21:04 +0900

    theme

---
+++ themes.js
@@ -0,0 +1,15 @@
+module.exports = {
+  hybrid: {
+    "string": "yellow",
+    "number": "red",
+    "boolean": 167,
+    "infinity": "red",
+    "nan": "cyan",
+    "null": "red",
+    "undefined": "gray",
+    "regexp": "green",
+    "key": "magenta",
+    "object": "white",
+    "array": "white"
+  }
+};
